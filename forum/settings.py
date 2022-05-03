b6c18324c36ed41b3c2c06647c1e02e7c2374bab
Samuel <sam_olayemi65@yahoo.com>
2022-05-04 00:00:22 +0100

    update

--- forum/settings.py
+++ forum/settings.py
@@ -124,15 +124,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
-
-MEDIA_ROOT = BASE_DIR / 'media'
-MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
+
+
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
