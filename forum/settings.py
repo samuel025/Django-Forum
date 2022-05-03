e3b922692282e2462aad74a9583b56a9b49b1a7a
Samuel <sam_olayemi65@yahoo.com>
2022-05-03 23:46:51 +0100

    UPDATE

--- forum/settings.py
+++ forum/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
