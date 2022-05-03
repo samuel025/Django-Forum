570f51ba32ca5b69a4c3bc927885aa66b7ad603b
Samuel <sam_olayemi65@yahoo.com>
2022-05-03 23:36:04 +0100

    update

--- forum/settings.py
+++ forum/settings.py
@@ -139,11 +139,10 @@ LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL = 'login'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('PASS')
-EMAIL_HOST_PASSWORD = os.environ.get('MAIL')
-
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 1025
+EMAIL_USE_TLS = False
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
 
 
