e43da46786602159dd9ced34790f137f3753baef
Samuel <sam_olayemi65@yahoo.com>
2022-05-04 00:53:49 +0100

    Ready for deployment

--- forum/settings.py
+++ forum/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 's@+09i2d*$(m#yn!1r-lg$wv(%t84(_a4c3m86@&$8dbq%(h^8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['samuel-blog.herokuapp.com', 'localhost']
 
 
 # Application definition
