9bf0fb4474fa6aad95607a5cef1c8e6a6f040ac6
Samuel <sam_olayemi65@yahoo.com>
2022-05-04 00:47:04 +0100

    update

--- forum/settings.py
+++ forum/settings.py
@@ -16,8 +16,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-import django_heroku
-django_heroku.settings(locals())
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -152,3 +151,6 @@ EMAIL_HOST_USER = ""
 EMAIL_HOST_PASSWORD = ""
 
 
+import dj_database_url 
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
